0e7db371576aa666e2f940aed80a2e1fdca01b9b
Aref Norouzi <caspiwebinfo@gmail.com>
2024-10-17 23:17:16 +0330

    db added

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core.blog'
 ]
 
 MIDDLEWARE = [
